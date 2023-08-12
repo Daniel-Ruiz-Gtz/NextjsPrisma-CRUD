4e2990b7bd03adc9dfae94313dec6c0a9027571b
Daniel-Ruiz-Gtz <58959667+Daniel-Ruiz-Gtz@users.noreply.github.com>
2023-08-12 15:31:09 -0600

    List Notes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
-function HomePage() {
-  return <div>HomePage</div>;
+import NoteForm from "@/components/NoteForm";
+
+async function loadNotes() {
+  const res = await fetch("http://localhost:3000/api/notes");
+  const data = await res.json();
+  return data;
+}
+
+async function HomePage() {
+  const notes = await loadNotes();
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div>
+        <NoteForm />
+        {notes.map((note) => (
+          <div key={note.id} className="bg-slate-400 p-4 rounded-md m-2">
+            <h1> {note.title} </h1>
+            <p> {note.content} </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
